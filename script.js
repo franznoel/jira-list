48c9d6e2622fa9c487a8dad4a0ee1f596fc74eb8
Franz Noel Tanglao <franz@asteriainteractive.com>
2017-07-11 15:43:36 -0700

    feat: Initialize Jira list table

---
+++ script.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+
+   // fetch('https://privemd.atlassian.net/rest/api/2/permissions/project',{
+   //  method: "POST"
+   // }).then(function(response) {
+   //  console.log(response);
+   // });
+
+});
